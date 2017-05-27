ac5a1dad185bd7c6c7016dcd61821fb1b600e172
benjaminVic <benjamin.vic.lallart@gmail.com>
2017-05-27 22:27:18 +0200

    More functions + insert tax correction

--- Projet APEX/function for pages.sql
+++ Projet APEX/function for pages.sql
@@ -22,28 +22,82 @@ End
 SELECT gam.GAM_LABEL, tmp.AUC_PRICE, pla.PLA_LABEL
 FROM GAME gam
 JOIN PLATFORM pla ON gam.FK_ID_PLATFORM = pla.ID_PLATFORM
-join (
+JOIN (
     SELECT MIN(AUC_PRICE), gam2.ID_GAME, auc2.AUC_PRICE
     FROM auction auc2
-    join game gam2 on auc2.FK_ID_GAME = gam2.ID_GAME
-    join AUC_CODE aco2 on aco2.FK_ID_AUCTION = auc2.ID_AUCTION
+    JOIN game gam2 on auc2.FK_ID_GAME = gam2.ID_GAME
+    JOIN AUC_CODE aco2 on aco2.FK_ID_AUCTION = auc2.ID_AUCTION
     WHERE aco2.ACO_SOLD = 0 
     group by gam2.ID_GAME, auc2.AUC_PRICE
 ) tmp on tmp.ID_GAME = gam.ID_GAME
 order by gam.GAM_LABEL;
 
 --DONEDONE
---COMPTE LE NOMBRE DE JEUX VENDU ET NON VENDU
+--STATS GLOBALE : COMPTE LE NOMBRE DE JEUX VENDU ET NON VENDU
 SELECT gam.GAM_LABEL, tmp.NB_NONVENDU, tmp.NB_VENDU, pla.PLA_LABEL
 FROM GAME gam
 JOIN PLATFORM pla ON gam.FK_ID_PLATFORM = pla.ID_PLATFORM
-join (
+JOIN (
     SELECT SUM(case when aco2.ACO_SOLD = 0 then 1 else 0 END) as NB_NONVENDU, 
            SUM(case when aco2.ACO_SOLD = 1 then 1 else 0 END) as NB_VENDU, 
            gam2.ID_GAME
     FROM auction auc2
-    join game gam2 on auc2.FK_ID_GAME = gam2.ID_GAME
-    join AUC_CODE aco2 on aco2.FK_ID_AUCTION = auc2.ID_AUCTION
+    JOIN game gam2 on auc2.FK_ID_GAME = gam2.ID_GAME
+    JOIN AUC_CODE aco2 on aco2.FK_ID_AUCTION = auc2.ID_AUCTION
     group by gam2.ID_GAME
 ) tmp on tmp.ID_GAME = gam.ID_GAME
 order by gam.GAM_LABEL;
+
+
+--DONEDONE
+--STATS PAR VENDEUR : SE PASER SUR PAGE PRECEDENTE POUR AVOIR STATISTIQUE VENTE PAR PERSONNE
+SELECT gam.GAM_LABEL, tmp.NB_NONVENDU, tmp.NB_VENDU, pla.PLA_LABEL, tmp.USR_LAST_NAME
+FROM GAME gam
+JOIN PLATFORM pla ON gam.FK_ID_PLATFORM = pla.ID_PLATFORM
+JOIN (
+    SELECT SUM(case when aco2.ACO_SOLD = 0 then 1 else 0 END) as NB_NONVENDU, 
+           SUM(case when aco2.ACO_SOLD = 1 then 1 else 0 END) as NB_VENDU, 
+           gam2.ID_GAME,
+           seller2.USR_LAST_NAME
+    FROM auction auc2
+    JOIN game gam2 on auc2.FK_ID_GAME = gam2.ID_GAME
+    JOIN AUC_CODE aco2 on aco2.FK_ID_AUCTION = auc2.ID_AUCTION
+    JOIN UTILISATEUR seller2 on seller2.ID_UTILISATEUR = auc2.FK_ID_SELLER
+    group by gam2.ID_GAME,seller2.USR_LAST_NAME
+) tmp on tmp.ID_GAME = gam.ID_GAME
+order by tmp.NB_VENDU;
+
+--DONEDONE
+--Stats de remboursement de clef par jeu et par personne
+SELECT tmp.USR_LAST_NAME ,tmp.NOMBRE_REMBOURSEMENT, gam.GAM_LABEL, pla.PLA_LABEL
+FROM GAME gam
+JOIN PLATFORM pla ON gam.FK_ID_PLATFORM = pla.ID_PLATFORM
+JOIN (
+    SELECT count(tra2.TRN_REFUND_DATE) as NOMBRE_REMBOURSEMENT, gam2.ID_GAME, seller2.USR_LAST_NAME
+    FROM auction auc2
+    JOIN game gam2 on auc2.FK_ID_GAME = gam2.ID_GAME
+    JOIN AUC_CODE aco2 on aco2.FK_ID_AUCTION = auc2.ID_AUCTION
+    JOIN TRANSACT tra2 on aco2.FK_ID_TRANSACT = tra2.ID_TRANSACT
+    JOIN UTILISATEUR seller2 on seller2.ID_UTILISATEUR = auc2.FK_ID_SELLER
+    WHERE aco2.ACO_SOLD = 1
+    AND tra2.TRN_REFUND_DATE is not null
+    group by gam2.ID_GAME, seller2.USR_LAST_NAME
+) tmp on tmp.ID_GAME = gam.ID_GAME
+order by tmp.NOMBRE_REMBOURSEMENT;
+
+--DONEDONE
+--STATS ACHAT ACHETEUR : nom/mail
+SELECT USR.USR_LAST_NAME, sum(TRN.TRN_TOTAL) as DEPENSE_TOTAL, avg(TRN.TRN_TOTAL) as DEPENSE_MOYENNE, USR.USR_MAIL
+FROM UTILISATEUR USR
+JOIN TRANSACT TRN on TRN.FK_ID_BUYER = USR.ID_UTILISATEUR
+WHERE TRN_REFUND_DATE is null
+AND TRN_DATE is not null
+GROUP BY USR.USR_LAST_NAME, USR.USR_MAIL;
+
+--DONEDONE
+--Stats de taxes à payer par etat
+SELECT COU.COU_LABEL, SUM(TRN.TRN_TAX_VALUE) 
+FROM COUNTRY COU
+JOIN TRANSACT TRN on TRN.FK_ID_COUNTRY = COU.ID_COUNTRY
+WHERE TRN.TRN_REFUND_DATE is null
+GROUP BY COU.COU_LABEL;
